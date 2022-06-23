f39829fe4274573ef4ee4cff6e142e78f46b3aee
cyrus01337 <cyrus21337@gmail.com>
2022-06-23 03:54:46 +0100

    Let's bring this to life, shall we?

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
---!strict
-export type Array<T> = {T}
-export type Mapping<K, V> = { [K]: V }
+local Types = {}
 
-return nil
+export type Array<V> = { [number]: V }
+export type Dictionary<V> = { [string]: V }
+export type Function<P..., R...> = (P...) -> R...
+export type Mapping<K, V> = { [K]: V }
+export type Table = Array<any> | Mapping<any, any>
+
+return Types
